6a393a38e214b08b0efd53ba01d8780a8895e599
Inna Babiichuk <dreambig@Innas-MacBook-Pro.local>
2019-12-22 14:30:54 -0800

    styling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,48 +1,61 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './../assets/style.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./../assets/style.css";
 import man from "./../i/man.png";
 
 function Dropdown() {
-    const wrapperRef = useRef(null);
-    const [ displayMenu, setState ] = useState(false);
+  const wrapperRef = useRef(null);
+  const [displayMenu, setState] = useState(false);
 
-    useEffect(() => {
-        document.addEventListener("click", handleClickOutside, false);
-        return () => {
-          document.removeEventListener("click", handleClickOutside, false);
-        };
-    }, []);
-    
-    const handleClickOutside = event => {
-        if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-            setState(false);
-        }
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, false);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, false);
     };
+  }, []);
 
-    const showDropdownMenu = () => {
-        setState(v => { console.log(v); return !v}); 
+  const handleClickOutside = event => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setState(false);
     }
-      
-    return (
-        <div className="navBtn" onClick={showDropdownMenu} ref={wrapperRef}>
-            <div className="profile-picture"><img src={man} alt="navOPen"/></div>
-            <div className= "userName">John Doe </div> 
-        
-            {displayMenu && (
-                <div className="dropdownContent">
-                    <ul> 
-                        <li className="userInfo">   
-                            <div className="profile-picture"><img src={man} alt="navOPen"/></div>
-                            <div className= "userName">John Doe</div> 
-                        </li>
-                        <li><a href="#Setting">Setting</a></li>
-                        <li><a href="#Setting">My Account</a></li>
-                        <li><a href="#Log Out">Log Out</a></li>
-                    </ul>
-                </div>
-            )}
+  };
+
+  const showDropdownMenu = () => {
+    setState(v => {
+      console.log(v);
+      return !v;
+    });
+  };
+
+  return (
+    <div className="navBtn" onClick={showDropdownMenu} ref={wrapperRef}>
+      <div className="profile-picture">
+        <img src={man} alt="navOPen" />
+      </div>
+      <div className="userName">John Doe </div>
+
+      {displayMenu && (
+        <div className="dropdownContent">
+          <ul>
+            <li className="userInfo">
+              <div className="profile-picture">
+                <img src={man} alt="navOPen" />
+              </div>
+              <div className="userName">John Doe</div>
+            </li>
+            <li>
+              <a href="#Setting">Setting</a>
+            </li>
+            <li>
+              <a href="#Setting">My Account</a>
+            </li>
+            <li>
+              <a href="#Log Out">Log Out</a>
+            </li>
+          </ul>
         </div>
-    )   
+      )}
+    </div>
+  );
 }
 
 export default Dropdown;
